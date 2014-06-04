fbc03e45661e18e2283718328ce54d4811696636
donal <donal@nashape.com>
2014-06-04 17:30:19 +0800

    add CLI args and a menu to choose which instance to ssh into
    I've also made the ssh script stand alone.

--- scripts/aws_hostname.sh
+++ scripts/aws_hostname.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# the the hostname of the first running AWS instance of the given environment
-# TODO pass this in
-AWS_ENV=testing
+AWS_ENV=${1:-testing}
 
-hostnames=$(aws ec2 describe-instances \
+hostname_string=$(aws ec2 describe-instances \
   --filters "Name=tag:environment,Values=$AWS_ENV" \
   --query 'Reservations[*].[Instances[0].State.Name,Instances[0].PublicDnsName]' \
   --output text  | grep running | awk '{print $2}')
 
-# just print the first one
-first_hostname=$(echo $hostnames | awk '{print $1}')
+declare -a hostnames=( $hostname_string )
 
-# and only if there is a value
-if [ -n "$first_hostname" ]; then
-  echo $first_hostname
-fi
+echo "Finding instances from the $AWS_ENV environment"
+
+for hostname in ${hostnames[@]} ; do
+  printf "${hostname}\n"
+done
 
--- scripts/ssh_aws.sh
+++ scripts/ssh_aws.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-# TODO pass this in
-EC2_INSTANCE_USER=ubuntu
+AWS_ENV=${1:-testing}
+EC2_INSTANCE_USER=${2:-ubuntu}
 
-hostname=$(./aws_hostname.sh)
+hostname_string=$(aws ec2 describe-instances \
+  --filters "Name=tag:environment,Values=$AWS_ENV" \
+  --query 'Reservations[*].[Instances[0].State.Name,Instances[0].PublicDnsName]' \
+  --output text  | grep running | awk '{print $2}')
+
+declare -a hostnames=( $hostname_string )
+
+# I was getting this from a separate script but it seems more practical to keep
+# this as standalone.
+# declare -a hostnames=$(./aws_hostname.sh $AWS_ENV)
+
+echo "Finding instances from the $AWS_ENV environment"
 
 # and only if there is a value
-if [ -n "$hostname" ]; then
-  ssh $EC2_INSTANCE_USER@$hostname
+if [ -n "$hostnames" ]; then
+
+  PS3="Type a number or 'q' to quit: "
+
+  select hostname in ${hostnames[@]} ; do
+    if [ -n "$hostname" ]; then
+        ssh $EC2_INSTANCE_USER@${hostname}
+    fi
+    break
+  done
+
 else
   echo "No running instance."
 fi
