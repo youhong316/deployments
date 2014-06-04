72f6b15ca452e4467b6abd0500ca16050be5bea2
donal <donal@nashape.com>
2014-06-04 17:38:01 +0800

    ssh in directly if there's only one instance

--- scripts/ssh_aws.sh
+++ scripts/ssh_aws.sh
@@ -3,12 +3,12 @@
 AWS_ENV=${1:-testing}
 EC2_INSTANCE_USER=${2:-ubuntu}
 
-hostname_string=$(aws ec2 describe-instances \
+hostnames_string=$(aws ec2 describe-instances \
   --filters "Name=tag:environment,Values=$AWS_ENV" \
   --query 'Reservations[*].[Instances[0].State.Name,Instances[0].PublicDnsName]' \
   --output text  | grep running | awk '{print $2}')
 
-declare -a hostnames=( $hostname_string )
+declare -a hostnames=( $hostnames_string )
 
 # I was getting this from a separate script but it seems more practical to keep
 # this as standalone.
@@ -19,14 +19,20 @@ echo "Finding instances from the $AWS_ENV environment"
 # and only if there is a value
 if [ -n "$hostnames" ]; then
 
-  PS3="Type a number or 'q' to quit: "
+  if [ "${#hostnames[@]}" -eq 1 ] ; then
+    ssh $EC2_INSTANCE_USER@${hostnames[0]}
+  fi
 
-  select hostname in ${hostnames[@]} ; do
-    if [ -n "$hostname" ]; then
-        ssh $EC2_INSTANCE_USER@${hostname}
-    fi
-    break
-  done
+  if [ "${#hostnames[@]}" -gt 1 ] ; then
+    PS3="Type a number or 'q' to quit: "
+
+    select hostname in ${hostnames[@]} ; do
+      if [ -n "$hostname" ]; then
+          ssh $EC2_INSTANCE_USER@${hostname}
+      fi
+      break
+    done
+  fi
 
 else
   echo "No running instance."
